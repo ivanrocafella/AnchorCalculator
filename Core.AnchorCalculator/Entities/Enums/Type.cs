82373664bf5cec876905361e3b63818e1693f7ff
Ivan Kobtsev <vanomc77@gmail.com>
2023-02-10 11:16:18 +0600

    Added Type enum

---
+++ Core.AnchorCalculator/Entities/Enums/Type.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.AnchorCalculator.Entities.Enums
+{
+    internal enum Type
+    {
+        A1_ST45,
+        A1_3PS,
+        A1_ST10_20,
+        A3_A500,
+        A3_35GS
+    }
+}
