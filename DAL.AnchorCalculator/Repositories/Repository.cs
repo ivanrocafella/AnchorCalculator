3bab592b69e621954a0e41ea439db19e400b6b49
Ivan Kobtsev <vanomc77@gmail.com>
2023-02-10 13:11:39 +0600

    Added DAL

---
+++ DAL.AnchorCalculator/Repositories/Repository.cs
@@ -0,0 +1,80 @@
+﻿using Core.AnchorCalculator.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.AnchorCalculator.Repositories
+{
+    internal class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+
+        protected ApplicationDbContext Context;
+        protected DbSet<TEntity> DbSet;
+        private bool _disposed;
+
+        public Repository(ApplicationDbContext context)
+        {
+            Context = context;
+            DbSet = Context.Set<TEntity>();
+        }
+        public void Add(TEntity obj) => DbSet.Add(obj);
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (_disposed)
+                return;
+
+            if (disposing)
+                Context.Dispose();
+
+            _disposed = true;
+        }
+
+        ~Repository()
+        {
+            Dispose(false);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        public IQueryable<TEntity> Find(Expression<Func<TEntity, bool>> predicate) => DbSet.Where(predicate);
+
+        public IQueryable<TEntity> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public TEntity GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveByEntity(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveRange(IEnumerable<TEntity> entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(TEntity obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
