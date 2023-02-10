bba782306578aca1677b0807cc072a9d36e4e934
Ivan Kobtsev <vanomc77@gmail.com>
2023-02-10 11:18:21 +0600

    Added Core

---
+++ Core.AnchorCalculator/Entities/Anchor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.AnchorCalculator.Entities
+{
+    internal class Anchor : Entity
+    {
+        public int LengthAnchor { get; set; }
+        public int DiameterAnchor { get; }
+        public double WeightAnchor { get; }
+        public double PriceAnchor { get; }
+        public int LengthBend { get; set; }
+        public int RadiusBend { get; }
+        public int LengthThread { get; set; }
+        public int DiameterThread { get; set; }
+        public double StepThread { get; }
+        public double PriceTotalAnchor { get; }
+        public int Quantity { get; }
+
+    }
+}
---
+++ Core.AnchorCalculator/Entities/Material.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.AnchorCalculator.Entities
+{
+    internal class Material : Entity
+    {
+        public int Size { get; set; }
+        public string? Type { get; set; }
+        public string? Name { get; }
+
+        public Material(int size, string? type)
+            => Name = $"Круг {size} {type}";
+    }
+}
